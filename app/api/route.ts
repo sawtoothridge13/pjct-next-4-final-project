3446d458f01822501b6566c6b7deeff9c6682b00
Doug Andrews <sawtoothridge13@gmail.com>
2023-07-28 11:00:08 +0200

    Revert "Add changes to trip management form"
    This reverts commit be234d30e9e84f1ba5ebf93773c9bb073d319673.

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
 
 export function GET(): NextResponse<{ trips: string }> {
-  return NextResponse.json({ trips: '/api/trips' });
+  return NextResponse.json({ trips: '/api/trips/' });
 }
